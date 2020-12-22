a045a7a6111061d95e67d8b8bc64aa0aaa61cb96
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-22 16:47:10 +0300

    Update services.blade.php

--- resources/views/frontend/sources/services.blade.php
+++ resources/views/frontend/sources/services.blade.php
@@ -1,5 +1,5 @@
 <!-- JIVOSITE online chat and callback -->
-<script src="//code.jivosite.com/widget/ZPbeJ8vlEb" async></script>
+<script src="//code.jivosite.com/widget/zOpqQSqXno" async></script>
 
 
 <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(68293105, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/68293105" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
