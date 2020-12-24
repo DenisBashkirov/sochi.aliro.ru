044478dda9225ab607ae9d8c8112197e0a8f6315
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-24 22:19:25 +0300

    Update footer.blade.php

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -3,21 +3,20 @@
     <div class="container flex xs-sm:flex-col xl:flex-row xl:justify-between xs-sm:sy-6">
 
         <div class="xs-sm:sy-2 xl:sy-4">
-            <h2 class="xs-sm:text-xl xl:text-2xl font-bold">Офисы продаж</h2>
+            <p class="xs-sm:text-xl xl:text-2xl font-bold">Офисы продаж</p>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
-                <p class="js-goto" data-goto="Krasnodar">{{ $company_contacts['address']['office']['krasnodar'] }}</p>
-                <a href="tel:{{ $company_contacts['phone']['office']['krasnodar'] }}" class="no-underline">{{ $company_contacts['phone']['office']['krasnodar'] }}</a>
+                <p>{{ $company_contacts['address']['office']['sochi'] }}</p>
+                <a href="tel:{{ $company_contacts['phone']['office']['sochi'] }}" class="no-underline">{{ $company_contacts['phone']['office']['sochi'] }}</a>
+            </div>
+            <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
+                <p class="" data-goto="Sochi">{{ $company_contacts['address']['office']['krasnodar'] }}</p>
             </div>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
                 <p class="" data-goto="Sochi">{{ $company_contacts['address']['office']['novoros'] }}</p>
             </div>
-            <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
-                <p class="" data-goto="Sochi">{{ $company_contacts['address']['office']['sochi'] }}</p>
-            </div>
         </div>
-
         <div class="xs-sm:sy-2 xl:sy-4">
-            <h2 class="xs-sm:text-xl xl:text-2xl font-bold">Производство</h2>
+            <p class="xs-sm:text-xl xl:text-2xl font-bold">Производство</p>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
                 <p>{{ $company_contacts['address']['production'] }}</p>
             </div>
